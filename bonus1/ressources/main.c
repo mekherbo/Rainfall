e6fddc6159ac1a01e7b9550f9cc42f630d5f6066
psyko944 <mehdi.kherboucheidf@gmail.com>
2026-08-31 05:25:16 +0200

    modify ressources for bonus1

--- bonus1/ressources/main.c
+++ bonus1/ressources/main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
-
+#include <stdint.h>
 int main(void)
 {
-	int n = INT_MIN;
+	int32_t n = INT32_MIN;
 	while (n * 4 < 44)
-	{
-		
 		n++;
-	}
 	printf("%d\n", n);
 	return 0;
 }
